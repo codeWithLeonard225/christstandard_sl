bfc6736b3ecf746aaecf488feb5b1123e3b01ee1
codeWithLeonard225 <leonardskargbo@gmail.com>
2026-09-20 23:35:03 -0600

    teacher dashboard

--- src/Component/TeacherAssignment/TeacherDashboard.jsx
+++ src/Component/TeacherAssignment/TeacherDashboard.jsx
@@ -63,11 +63,11 @@ const getNavItems = (teacherInfo) => {
       label: `Form Class: ${teacherInfo.assignClass || "N/A"}`,
       icon: <MdLibraryBooks />,
       children: [
-        { key: "FormMasterGradeSheet", label: "Submitted Grades" },
-        { key: "GradeSheet", label: "GradeSheet" },
-        { key: "TermResult", label: "Term Sheet" },
-        { key: "YearlyResult", label: "Yearly Result" },
-        { key: "ReportCard", label: "Report Card" },
+        // { key: "FormMasterGradeSheet", label: "Submitted Grades" },
+        // { key: "GradeSheet", label: "GradeSheet" },
+        // { key: "TermResult", label: "Term Sheet" },
+        // { key: "YearlyResult", label: "Yearly Result" },
+        // { key: "ReportCard", label: "Report Card" },
         { key: "PupilAttendanceLogs", label: "Pupil AttendanceLogs " },
       ],
     });
